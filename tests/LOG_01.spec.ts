ca62380f6864bb155312b2677c01447e90560614
Akio Ckist <doublekill0102030405@gmail.com>
2025-12-05 22:24:59 +0700

    Add login functionality tests and error handling in Playwright

--- tests/LOG_01.spec.ts
+++ tests/LOG_01.spec.ts
@@ -2,14 +2,14 @@ import test from "@playwright/test";
 import { HomePage } from "../pages/home.page";
 import { LoginPage } from "../pages/login.page";
 
-test('Verify navigation to Login page from Home page', async ({ page }) => {
+test('User can login successfully with valid account', async ({ page }) => {
     const homePage = new HomePage(page);
     const loginPage = new LoginPage(page);
     const email = 'cijnuj@ramcloud.us';
+    const password = '123456789';
 
     await page.goto('http://railwayb2.somee.com/Page/HomePage.cshtml');
-
     await homePage.navigateToLogin();
-    await loginPage.login(email, '123456789'); 
+    await loginPage.login(email, password);
     await homePage.shouldWelcomeMsgVisible(email);
 });
--- tests/LOG_02.spec.ts
+++ tests/LOG_02.spec.ts
@@ -2,14 +2,15 @@ import test from "@playwright/test";
 import { HomePage } from "../pages/home.page";
 import { LoginPage } from "../pages/login.page";
 
-test('Verify navigation to Login page from Home page', async ({ page }) => {
+test('An error message is displayed when user login with non-existent account', async ({ page }) => {
     const homePage = new HomePage(page);
     const loginPage = new LoginPage(page);
-    const email = 'cijnuj@ramcloud.us';
+    const nonExistentEmail = 'nonexistent@example.com';
+    const password = '123456789';
 
     await page.goto('http://railwayb2.somee.com/Page/HomePage.cshtml');
 
     await homePage.navigateToLogin();
-    await loginPage.login(email, '123456789'); 
-    await homePage.shouldWelcomeMsgVisible(email);
+    await loginPage.login(nonExistentEmail, password);
+    await loginPage.shouldErrorMessageVisible();
 });
