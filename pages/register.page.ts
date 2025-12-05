36c9566677519cfaa0cd5cbde959c5a0789f51a6
3HoTuan3 <tuanho2389@gmail.com>
2025-12-05 23:01:10 +0700

    Done message displays when user tries to register with using existed email

--- pages/register.page.ts
+++ pages/register.page.ts
@@ -7,6 +7,7 @@ export class RegisterPage {
     private readonly confirmPasswordTxt: Locator;
     private readonly pidTxt: Locator;
     private readonly registerBtn: Locator;
+    private readonly errorMsg: Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -16,6 +17,8 @@ export class RegisterPage {
         this.confirmPasswordTxt = this.page.locator('#confirmPassword');
         this.pidTxt = this.page.getByLabel('PID');
         this.registerBtn = this.page.getByRole('button', { name: /register/i });
+
+        this.errorMsg = this.page.locator('div#content p.message.error');
     }
 
     async register(username: string, password: string, confirmPassword: string, pid: string): Promise<void> {
@@ -28,5 +31,14 @@ export class RegisterPage {
 
     async checkRegisterSuccess(): Promise<void> {
         await this.page.waitForSelector('div#content:has-text("You\'re here")', { state: 'visible' });
+    }
+
+    async getRegisterAlreadyUseText(): Promise<string> {
+        await this.errorMsg.waitFor({ state: 'visible', timeout: 5000 });
+        return (await this.errorMsg.textContent()) ?? '';
+    }
+
+    async isRegisterErrorVisible(): Promise<boolean> {
+        return await this.errorMsg.isVisible().catch(() => false);
     }
 }
