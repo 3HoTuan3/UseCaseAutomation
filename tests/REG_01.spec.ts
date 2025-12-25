39a4a871aa5261d58c8d820884f6cef27b9aa52c
3HoTuan3 <tuanho2389@gmail.com>
2025-12-25 11:20:47 +0700

    Refactor registration tests to use User model for user creation and improve step organization

--- tests/REG_01.spec.ts
+++ tests/REG_01.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker';
 import test from "@playwright/test";
 import { HomePage } from "../pages/home.page";
 import { RegisterPage } from "../pages/register.page";
+import { User } from "../models/user";
 
 test('REG_01 - Register new account', async ({ page }) => {
     const homePage = new HomePage(page);
@@ -10,9 +11,13 @@ test('REG_01 - Register new account', async ({ page }) => {
     const email = faker.internet.email();
     const password = faker.internet.password();
     const PID = faker.string.numeric(9);
+    const user = new User({ username: email, password: password, confirmPassword: password, pid: PID });
 
     await homePage.navigateToHomePage();
     await homePage.navigateToRegister();
-    await registerPage.register(email, password, password, PID);
+    
+    test.step(`Register with email: ${email}`, async () => {
+        await registerPage.register(user);
+    });
     await registerPage.checkRegisterSuccess();
 });
--- tests/REG_02.spec.ts
+++ tests/REG_02.spec.ts
@@ -1,5 +1,6 @@
 import test, { expect } from "@playwright/test";
 import { HomePage } from "../pages/home.page";
+import { User } from "../models/user";
 import { RegisterPage } from "../pages/register.page";
 import { faker } from "@faker-js/faker";
 
@@ -10,19 +11,27 @@ test('REG_02 - Error when registering with an existing email', async ({ page }) 
     const email = faker.internet.email();
     const password = faker.internet.password();
     const PID = faker.string.numeric(9);
+    const user = new User({ username: email, password: password, confirmPassword: password, pid: PID });
 
     await homePage.navigateToHomePage();
     await homePage.navigateToRegister();
 
     // 1) register first time
-    await registerPage.register(email, password, password, PID);
+    await test.step(`Register with email: ${email}`, async () => {
+        await registerPage.register(user);
+    });
+
     await registerPage.checkRegisterSuccess();
 
     // 2) try to register again with same email
     await homePage.navigateToRegister();
-    await registerPage.register(email, password, password, PID);
+    await test.step(`Register with email: ${email}`, async () => {
+        await registerPage.register(user);
+    });
 
     // assert error shown
-    const err = await registerPage.getRegisterAlreadyUseText();
-    expect(err).toMatch(/already|exists|in use/i);
+    await test.step("Check error message is shown for existing email", async () => {
+        const err = await registerPage.getRegisterAlreadyUseText();
+        expect(err).toMatch(/already|exists|in use/i);
+    });
 });
