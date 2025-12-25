89339f3744d267e6d563335615fbdb4ff9a0a38f
3HoTuan3 <tuanho2389@gmail.com>
2025-12-25 09:53:51 +0700

    Refactor navigation methods to include test steps for better visibility in Playwright

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,4 +1,4 @@
-import { expect, Locator, Page } from "@playwright/test";
+import { test, expect, Locator, Page } from "@playwright/test";
 
 export class HomePage {
   private readonly page: Page;
@@ -20,23 +20,33 @@ export class HomePage {
   }
 
   async navigateToHomePage(): Promise<void> {
-    await this.page.goto("http://railwayb2.somee.com/Page/HomePage.cshtml");
+    await test.step("Navigate to Home Page", async () => {
+      await this.page.goto("http://railwayb2.somee.com/Page/HomePage.cshtml");
+    });
   }
 
   async navigateToLogin(): Promise<void> {
-    await this.navBarLogin.click();
+    await test.step("Navigate to Login Page", async () => {
+      await this.navBarLogin.click();
+    });
   }
 
   async navigateToChangePassword(): Promise<void> {
-    await this.navChangePassword.click();
+    await test.step("Navigate to Change Password Page", async () => {
+      await this.navChangePassword.click();
+    });
   }
 
   async navigateToRegister(): Promise<void> {
-    await this.navRegister.click();
+    await test.step("Navigate to Register Page", async () => {
+      await this.navRegister.click();
+    });
   }
 
   async logout(): Promise<void> {
-    await this.navLogout.click();
+    await test.step("Logout", async () => {
+      await this.navLogout.click();
+    });
   }
 
   // Thay đổi: nhận email (tuỳ chọn) và kiểm tra welcome message
