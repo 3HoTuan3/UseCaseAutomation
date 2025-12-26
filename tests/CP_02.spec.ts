d438e8585856252857c0fcb289e2e6bd87166dd5
tndot02 <nnguyen.tuan0205@gmail.com>
2025-12-26 23:15:23 +0700

    Fix all Change Password test cases

--- tests/CP_02.spec.ts
+++ tests/CP_02.spec.ts
@@ -6,42 +6,48 @@ import { faker } from "@faker-js/faker";
 import { RegisterPage } from "../pages/register.page";
 import { User } from "../models/user";
 
-test("Verify user can change password with valid info", async ({ page }) => {
-  const homePage = new HomePage(page);
-  const loginPage = new LoginPage(page);
-  const registerPage = new RegisterPage(page);
-  const changePasswordPage = new ChangePasswordPage(page);
+const lengthCases = [8, 50, 64];
 
-  const tempUsername = faker.internet.email();
-  const tempPassword = faker.internet.password();
-  const tempNewPassword = faker.internet.password();
-  const tempPID = faker.string.numeric(9);
-  const user = new User({
-    username: tempUsername,
-    password: tempPassword,
-    confirmPassword: tempPassword,
-    pid: tempPID,
+for (const lengthCase of lengthCases) {
+  test(`Verify user can change password with valid info and password length ${lengthCase}`, async ({
+    page,
+  }) => {
+    const homePage = new HomePage(page);
+    const loginPage = new LoginPage(page);
+    const registerPage = new RegisterPage(page);
+    const changePasswordPage = new ChangePasswordPage(page);
+
+    const tempUsername = faker.internet.email();
+    const tempPassword = faker.internet.password();
+    const tempNewPassword = faker.string.alphanumeric(lengthCase);
+    const tempPID = faker.string.numeric(9);
+    const user = new User({
+      username: tempUsername,
+      password: tempPassword,
+      confirmPassword: tempPassword,
+      pid: tempPID,
+    });
+
+    await homePage.navigateToHomePage();
+    await homePage.navigateToRegister();
+    await registerPage.register(user);
+    await homePage.navigateToLogin();
+    await loginPage.login(user);
+
+    // Change password
+    await homePage.navigateToChangePassword();
+    await changePasswordPage.changePassword(
+      user,
+      tempNewPassword,
+      tempNewPassword,
+    );
+
+    // Update password in user object
+    user.password = tempNewPassword;
+
+    // Re-login with new password to verify
+    await homePage.logout();
+    await homePage.navigateToLogin();
+    await loginPage.login(user);
   });
-
-  await homePage.navigateToHomePage();
-  await homePage.navigateToRegister();
-  await registerPage.register(user);
-  await homePage.navigateToLogin();
-  await loginPage.login(user);
-
-  // Change password
-  await homePage.navigateToChangePassword();
-  await changePasswordPage.changePassword(
-    user,
-    tempNewPassword,
-    tempNewPassword,
-  );
-
-  // Update password in user object
-  user.password = tempNewPassword;
-
-  // Re-login with new password to verify
-  await homePage.logout();
-  await homePage.navigateToLogin();
-  await loginPage.login(user);
-});
+}
