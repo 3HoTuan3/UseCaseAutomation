7f018b02b55f0128ecb64ce7c16eba444da4e224
Akio Ckist <doublekill0102030405@gmail.com>
2025-12-26 22:51:37 +0700

    Refactor test name for clarity in HOM_04.spec.ts

--- tests/HOM_04.spec.ts
+++ tests/HOM_04.spec.ts
@@ -2,7 +2,7 @@ import { test } from "@playwright/test";
 import { HomePage } from "../pages/home.page";
 import { User } from "../models/user";
 
-test("HOM_04 - Additional tab displays and work properly when user is logged in", async ({
+test("Additional tab displays and work properly when user is logged in", async ({
   page,
 }) => {
   const homePage = new HomePage(page);
