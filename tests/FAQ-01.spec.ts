bbf6de654d77f211310c0fda545053249c521849
Akio Ckist <doublekill0102030405@gmail.com>
2025-12-27 10:05:27 +0700

    Refactor FAQ tests to streamline login and navigation steps

--- tests/FAQ-01.spec.ts
+++ tests/FAQ-01.spec.ts
@@ -1,30 +1,10 @@
 import { test } from "@playwright/test";
 import { FaqPage } from "../pages/faq.page";
-import { HomePage } from "../pages/home.page";
-import { LoginPage } from "../pages/login.page";
 import { User } from "../models/user";
 
 test("FAQ-01 - Verify FAQ page displays correctly", async ({ page }) => {
   const faqPage = new FaqPage(page);
-  const homePage = new HomePage(page);
-  const loginPage = new LoginPage(page);
   const user = new User();
 
-  await test.step("Login to Railway", async () => {
-    await homePage.navigateToHomePage();
-    await homePage.navigateToLogin();
-    await loginPage.login(user);
-    await homePage.shouldWelcomeMsgVisible(user.username);
-  });
-
-  await test.step("Click on FAQ menu in navigation bar", async () => {
-    await homePage.navigateToFaq();
-  });
-
-  await test.step("Observe the page content", async () => {
-    await faqPage.verifyFaqPageLoaded();
-    await faqPage.shouldFaqTitleVisible();
-    await faqPage.shouldFaqQuestionsVisible();
-    await faqPage.shouldFaqAnswersVisible();
-  });
+  await faqPage.loginAndNavigateToFaq(user);
 });
